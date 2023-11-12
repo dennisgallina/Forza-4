583a0d74345cf0cd0feecf64ee34c97204c3ba59
Dennis Gallina <dennis.gallina@gmail.com>
2023-11-12 23:37:14 +0100

    Correzione

--- Server/src/RichiestaDalClient.java
+++ Server/src/RichiestaDalClient.java
@@ -1,15 +1,14 @@
 public class RichiestaDalClient {
-    public String comando; // Azione richiesta
-    public int posizioneX; // Colonna scelta dal giocatore
+    public String command; // Azione richiesta
+    public int positionX; // Colonna scelta dal giocatore
 
     public RichiestaDalClient(String request) {
         String[] parametriRichiesta = request.split(";");
 
-        this.comando = parametriRichiesta[0]; // Prende il comando da eseguire
+        this.command = parametriRichiesta[0]; // Prende il comando da eseguire
 
         // Nel caso il comando sia di posizionare la pedina inserisce la colonna
-        if (parametriRichiesta.length < 2) 
-            this.posizioneX = Integer.parseInt(parametriRichiesta[1]);
-            
+        if (parametriRichiesta.length >= 2) 
+            this.positionX = Integer.parseInt(parametriRichiesta[1]);
     }
 }
