1595df4144383bd8c957e860fc5c05f3e896950b
Dennis Gallina <dennis.gallina@gmail.com>
2023-11-12 23:35:39 +0100

    Aggiunto l'inserimento della pedina

--- Server/src/Game.java
+++ Server/src/Game.java
@@ -1,24 +1,39 @@
+import java.io.IOException;
+
 public class Game {
     public boolean state; // Stato della partita: true -> in corso; false -> non in corso
     public PlayGround playGround; // Griglia di gioco
     public ThreadClient player1; // Giocatore 1
     public ThreadClient player2; // Giocatore 2
-    public char turn; // Turno di quale giocatore
+    public ThreadClient currentPlayer; // Giocatore corrente
 
     public Game() {
         this.state = false;
         this.playGround = new PlayGround();
         this.player1 = null;
         this.player2 = null;
-        this.turn = '0';
+        this.currentPlayer = null;
     }
 
-    public boolean insertPawn(String color, int positionX) {
-        if (playGround.insert(color, positionX) == true) 
-            return true;
-        else 
-            return false;
+    public void start() {
+        this.state = true;
+        this.currentPlayer = player1; // Inizia il giocatore 1
     }
 
-    public boolean 
+    public synchronized boolean insertPawn(String color, int positionX) throws IOException {
+        // Controlla se è il turno del giocatore corretto
+        if ((currentPlayer == player1 && color.equals("red")) || (currentPlayer == player2 && color.equals("yellow"))) {
+            if (playGround.insert(color, positionX)) {
+                // Cambia turno
+                currentPlayer = (currentPlayer == player1) ? player2 : player1;
+    
+                // Invia lo stato del gioco a entrambi i client
+                player1.send(playGround.getGamePlayGround());
+                player2.send(playGround.getGamePlayGround());
+    
+                return true;
+            }
+        }
+        return false;
+    }    
 }
