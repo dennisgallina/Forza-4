d3f841aa1e8e818e7fe02e4783a07e240c71284e
Dennis Gallina <dennis.gallina@gmail.com>
2023-11-13 17:58:58 +0100

    Aggiunto il Gestore delle Connessioni

--- Server/src/ServerTCP.java
+++ Server/src/ServerTCP.java
@@ -3,24 +3,34 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ServerTCP {
-    // Dichiarazione di un ServerSocket
-    private ServerSocket serverSocket;
+    public ServerSocket serverSocket; // Socket del Server
+    public ConnectionsManager connectionsManager; // Gestore delle connessioni create
 
-    // Costruttore della classe ServerTCP
     public ServerTCP(int porta) throws IOException {
-        // Inizializza il ServerSocket sulla porta specificata
         this.serverSocket = new ServerSocket(porta);
+        this.connectionsManager = new ConnectionsManager();
     }
 
-    // Metodo per accettare una connessione dal client
-    public Socket accettaConnessione() throws IOException {
+    // Accetta una connessione dal client
+    public Socket acceptConnection() throws IOException {
         // Accetta una connessione dal client e la restituisce
-        return serverSocket.accept();
+        Socket socket = serverSocket.accept();
+        connectionsManager.addConnection(socket);
+        return socket;
     }
 
-    // Metodo per chiudere il server
-    public void chiudi() throws IOException {
-        // Chiude il ServerSocket
-        serverSocket.close();
+    // Chiude le connessioni e il Server
+    public void close() throws IOException {
+        connectionsManager.closeAll(); // Chiude tutte le connesioni con i client
+        if (!serverSocket.isClosed()) 
+            serverSocket.close(); // Chiude la socket del server
+    }
+
+    public Socket getPlayer1Connection() {
+        return connectionsManager.connections.size() > 0 ? connectionsManager.connections.get(0) : null;
+    }
+
+    public Socket getPlayer2Connection() {
+        return connectionsManager.connections.size() > 1 ? connectionsManager.connections.get(1) : null;
     }
 }
