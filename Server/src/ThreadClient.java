76f8aef44005191c44d28a76abf33c7158f26db6
Dennis Gallina <dennis.gallina@gmail.com>
2023-11-13 18:00:04 +0100

    Rimossi i metodi di gestione della richiesta del Client e spostati nel Game

--- Server/src/ThreadClient.java
+++ Server/src/ThreadClient.java
@@ -5,33 +5,20 @@ import java.io.PrintWriter;
 import java.net.Socket;
 
 public class ThreadClient extends Thread {
-    private Socket socketClient; // Il socket del client
-    private Player player; // Il giocatore associato a questo thread
-    private Game game; // Il gioco a cui partecipa il giocatore
+    public Player player; // Il giocatore associato a questo thread
+    public Socket socketClient; // Il socket del client
+    public ClientRequest clientRequest; // Richiesta del client
 
-    public ThreadClient(Socket socketClient, Player player, Game game) {
-        this.socketClient = socketClient; 
+    public ThreadClient(Socket socketClient, Player player) {
         this.player = player; 
-        this.game = game; 
+        this.socketClient = socketClient; 
     }
 
     public void run() {
-        try {
-            // Crea un BufferedReader per leggere i dati in arrivo dal client
-            BufferedReader in = new BufferedReader(new InputStreamReader(socketClient.getInputStream()));
-        
-            String request;
-            // Continua a leggere le richieste dal client finché non riceve "disconnect" (cioè il client si disconnette)
-            while ((request = in.readLine()) != "disconnect") {
-                // Gestisce la richiesta del client
-                manageRequest(new RichiestaDalClient(request));
-            }
-        
-            send("disconnection confirmed");
-
-            // Chiude il BufferedReader e il socket del client
-            in.close();
-            socketClient.close();
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(socketClient.getInputStream()))) {
+            // Continua a leggere le richieste dal client finché la socket non viene chiusa
+            while (!socketClient.isClosed()) 
+                clientRequest = new ClientRequest(in.readLine()); // Gestisce la richiesta del client
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -40,16 +27,9 @@ public class ThreadClient extends Thread {
     // Metodo per inviare un messaggio al client
     public void send(String message) throws IOException {
         // Crea un PrintWriter per inviare dati al client
-        PrintWriter out = new PrintWriter(socketClient.getOutputStream(), true);
-        // Invia il messaggio al client
-        out.println(message);
-    }
-
-    // Metodo per gestire le richieste dal client
-    public void manageRequest(RichiestaDalClient richiestaDalClient) throws IOException {
-        // Se il comando è "insert", inserisce una pedina nel gioco
-        if (richiestaDalClient.command.equals("insert")) {
-            game.insertPawn(player.color, richiestaDalClient.positionX);
-        } 
+        try (PrintWriter out = new PrintWriter(socketClient.getOutputStream(), true)) {
+            // Invia il messaggio al client
+            out.println(message);
+        }
     }
 }
