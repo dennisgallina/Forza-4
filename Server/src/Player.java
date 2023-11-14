9af72aa743ec7e3ca8447a882a1715a194334e49
Dennis Gallina <dennis.gallina@gmail.com>
2023-11-14 16:21:54 +0100

    Rimozione costruttore di default.

--- Server/src/Player.java
+++ Server/src/Player.java
@@ -3,12 +3,6 @@ public class Player {
     public String color; // Colore delle pedine
     public boolean isLogged; // Stato della connessione: true -> connesso; false -> non connesso
 
-    public Player() {
-        this.name = null;
-        this.color = null;
-        this.isLogged = false;
-    }
-
     public Player(String name, String color) {
         this.name = name;
         this.color = color;
