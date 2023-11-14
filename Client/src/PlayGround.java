d05df1c91fe28637d7a77a97f0c92858cfdc9d84
Dennis Gallina <dennis.gallina@gmail.com>
2023-11-14 15:12:24 +0100

    Apportate le seguenti modifiche: - Migliorata sintassi; - Rimozione attributo superficiale.

--- Client/src/PlayGround.java
+++ Client/src/PlayGround.java
@@ -3,23 +3,21 @@ import java.util.List;
 
 public class PlayGround {
     public List<Pawn> pawns; // Pedine all'interno della griglia
-    public int[] columnHeights; // Altezza di ogni colonna
     public int columns; // Numero di colonne della griglia
     public int rows; // Numero di righe della griglia
 
     public PlayGround() {
         this.pawns = new ArrayList<>();
-        this.columnHeights = new int[7];
         this.columns = 6;
         this.rows = 7;
     }
 
-    public void insert(Pawn p)
+    public void insert(Pawn pawn)
     {
-        pawns.add(p);
+        pawns.add(pawn);
     }
 
-    public void fine()
+    public void clear()
     {
         pawns.clear();
     }
