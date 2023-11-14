0e46a8e75c6771bc5fb2d8a4a42421dcb3bdbbde
Dennis Gallina <dennis.gallina@gmail.com>
2023-11-14 15:10:44 +0100

    Sostituta della vecchia classe RichiestaAlServer

---
+++ Client/src/RequestAtServer.java
@@ -0,0 +1,7 @@
+public class RequestAtServer {
+    public String comando; // Azione richiesta
+    public int posizioneX; // Colonna scelta dal giocatore
+
+    public RequestAtServer() {
+    }
+}
