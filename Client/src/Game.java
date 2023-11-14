027c0f9e050f86ea5b04ae0ec3d3004d5e245669
Dennis Gallina <dennis.gallina@gmail.com>
2023-11-14 15:37:30 +0100

    Modifiche: - Migliorata sintassi; - Aggiunto il caso "wait"; - Aggiunti commenti.

--- Client/src/Game.java
+++ Client/src/Game.java
@@ -11,39 +11,44 @@ public class Game {
 
     public void manageDataFromServer() throws IOException
     {
-        while(true)
+        while (true)
         {
             String[] dataFromServer = clientTCP.data.split(";"); // Dati ricevuti dal Server
 
-            // Divisione dei dati in Pawns e Winner
-            String[] pawns;
-            String winner = "";
-            if (!dataFromServer[dataFromServer.length].equals("0") && !dataFromServer[dataFromServer.length].equals("1") && !dataFromServer[dataFromServer.length].equals("2")) {
-                pawns = dataFromServer;
-                pawns[pawns.length] = null;
-                winner = dataFromServer[dataFromServer.length];
-            } else
-                pawns = dataFromServer;
+            // Gestione della risposta del Server
+            if (dataFromServer[0].equals("wait")) { // Se il Server invia il comando di attesa
+                // Gestisce l'attesa della connessione per l'altro avversario
+            } else {
+                // Divisione dei dati in Pawns e Winner
+                String[] pawns;
+                String winner = "";
+                if (!dataFromServer[dataFromServer.length].equals("0") && !dataFromServer[dataFromServer.length].equals("1") && !dataFromServer[dataFromServer.length].equals("2")) {
+                    pawns = dataFromServer;
+                    pawns[pawns.length] = null;
+                    winner = dataFromServer[dataFromServer.length];
+                } else
+                    pawns = dataFromServer;
 
-            insertPawns(pawns);
+                insertPawns(pawns);
 
-            if(winner.equals("Player 1")) 
-                clientTCP.close();
-            else if(winner.equals("Player 2"))
-                clientTCP.close();
+                if (winner.equals("Player 1") || winner.equals("Player 2")) 
+                    clientTCP.close();
+            }
         }
     }
 
-    public void insertPawns(String[] vett)
+    // Inserisce le Pawns ricevute dal Server: 0 -> Pawn non presente, 1 -> Pawn red, 2 -> Pawn yellow
+    public void insertPawns(String[] pawns)
     {
-        for(int row = 1; row < playGround.rows; row++)
-        {
-            for(int column = 1, countPawns = 0; column < playGround.columns; column++, countPawns++)
-            {
-                if(vett[countPawns].equals("0")){}
-                else if(vett[countPawns].equals("1")){
-                    playGround.insert(new Pawn("yellow", column, row));
-                }
+        playGround.clear(); // Svuota il campo attuale per non sovrapporre l'inserimento
+
+        for (int row = 1; row < playGround.rows; row++) { // Scorre le righe dal basso verso l'alto 
+            for (int column = 1, countPawns = 0; column < playGround.columns; column++, countPawns++) { // Scorre le colonne della riga
+                if (pawns[countPawns].equals("1")) {
+                    playGround.insert(new Pawn("red", column, row)); // Inserimento Pawn red
+                } else if (pawns[countPawns].equals("2"))
+                    playGround.insert(new Pawn("yellow", column, row)); // Inserimento Pawn yellow
+
             }
         }
     }
