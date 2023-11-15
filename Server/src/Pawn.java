a9a582e842186c9918eb217c2e9e541bc2a2b984
Dennis Gallina <dennis.gallina@gmail.com>
2023-11-15 12:18:16 +0100

    Rimosso costruttore di default.

--- Server/src/Pawn.java
+++ Server/src/Pawn.java
@@ -3,12 +3,6 @@ public class Pawn {
     public int positionX; // Posizione orizzontale nella griglia
     public int positionY; // Posizione verticale nella griglia
 
-    public Pawn() {
-        this.color = null;
-        this.positionX = 0;
-        this.positionY = 0;
-    }
-
     public Pawn(String color, int positionX, int positionY) {
         this.color = color;
         this.positionX = positionX;
