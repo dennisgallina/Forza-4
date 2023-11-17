87b140e1c17e24f34f1ee823a69f5eeb1dfc1ba3
Dennis Gallina <dennis.gallina@gmail.com>
2023-11-17 19:37:50 +0100

    Modifiche: - Game diventa un Thread; - Miglioramento gestione della risposta del Server.

--- Client/src/Game.java
+++ Client/src/Game.java
@@ -1,54 +1,77 @@
 import java.io.IOException;
 
-public class Game {
+public class Game extends Thread {
     public Graphic graphic;
     public PlayGround playGround; // Griglia di gioco
     public ClientTCP clientTCP; // Comunicazione col Server
-    public RequestAtServer req;
+    public RequestAtServer requestAtServer; // Richiesta al Server
 
     public Game() throws IOException {
         this.playGround = new PlayGround();
         this.clientTCP = new ClientTCP("null", 0);
         this.graphic = new Graphic();
-        this.req = new RequestAtServer();
+        this.requestAtServer = new RequestAtServer();
     }
 
-    public void manageDataFromServer() throws IOException
-    {
-        boolean prem = false;
+    public void run() {
+        boolean isPressed = false;
+
         graphic.createLobby();
-        do{
-            prem = graphic.isButtonConnectPressed();
-        } while(!prem);
-        req.sendAtServer("", clientTCP, 0);
-        while (true)
-        {
-            if(prem = graphic.isButtonDiconnectPressed())
-                req.sendAtServer("disconnect", clientTCP, 0);
-            
-            String[] dataFromServer = clientTCP.data.split(";"); // Dati ricevuti dal Server
-            switch (dataFromServer[0]) {
-                case "wait":
-                    graphic.createWaitingScreen();
-                    break;
-                case "start":
-                    graphic.createCamp(playGround.rows, playGround.columns);
-                    break;
-                case "refresh":
-                    insertPawns(dataFromServer);
-                    
-                    break;
-                case "finish":
-                    graphic.Disconnect();
-                    break;
-                case "winner":
-                    graphic.WinnerScreenCreator();
-                    clientTCP.close();
-                    break;
-                default:
-                graphic.messagError();
-                    break;
+        do {
+            isPressed = graphic.isButtonConnectPressed();
+        } while (!isPressed);
+
+        while (true) {
+            if (isPressed = graphic.isButtonDiconnectPressed()) {
+                requestAtServer.command = "disconnect";
+                try {
+                    clientTCP.send(requestAtServer.command);
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
             }
+        }
+    }
+
+    // Gestione della risposta da parte del Server
+    public void manageResponse() throws IOException {
+        String[] dataFromServer = clientTCP.data.split(";"); // Dati ricevuti dal Server: [0] -> command; [1] -> Pawns/Winner
+        String command = dataFromServer[0]; // Comando ricevuto dal Server
+        // Gestione del comando ricevuto dal Server
+        switch (command) {
+            // Conferma della connessione
+            case "connection accepted":
+                graphic.createWaitingScreen();
+                break;
+            // Attesa dell'avversario
+            case "wait":
+                graphic.createWaitingScreen();
+                break;
+            // Inizio Game
+            case "start":
+                graphic.createCamp(playGround.rows, playGround.columns);
+                break;
+            // Aggiornamento del PlayGround
+            case "refresh":
+                String[] pawns = dataFromServer[1].split(","); // Pawns sono divise dalla virgola
+                insertPawns(pawns); // Inserisce le Pawns nel campo da gioco
+                break;
+            // Fine Game (l'avversario si è disconnesso)
+            case "finish":
+                graphic.Disconnect();
+                break;
+            // Esito vincitore, di conseguenza fine Game
+            case "winner":
+                graphic.WinnerScreenCreator();
+                clientTCP.close();
+                break;
+            // Non riconosciuto
+            case "command not recognized":
+                graphic.messagError();
+                break;
+            default:
+                break;
         }
     }
 
