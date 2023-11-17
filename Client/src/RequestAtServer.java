fadfd3dc7118026dca0723ddd70ee850885adca2
Dennis Gallina <dennis.gallina@gmail.com>
2023-11-17 19:38:56 +0100

    Modifica: Rimozione metodo send, ClientTCP si occupa di inviare al Server.

--- Client/src/RequestAtServer.java
+++ Client/src/RequestAtServer.java
@@ -1,30 +1,11 @@
 import java.io.IOException;
 
 public class RequestAtServer {
-    public String comando; // Azione richiesta
-    public int posizioneX; // Colonna scelta dal giocatore
+    public String command; // Azione richiesta
+    public int positionX; // Colonna scelta dal giocatore
 
     public RequestAtServer() {
-        this.comando = "";
-        this.posizioneX = 0;
-    }
-
-    public void sendAtServer(String comando, ClientTCP tcp, int positionX) throws IOException
-    {
-        switch (comando) {
-            case "":
-                tcp.start();
-                break;
-            case "insert":
-                String cmd = comando + ";" + positionX;
-                tcp.send(cmd);
-                break;
-            case "disconnect":
-                tcp.send(comando);
-                tcp.close();
-            default:    
-                tcp.send(comando);
-                break;
-        }
+        this.command = "";
+        this.positionX = 0;
     }
 }
