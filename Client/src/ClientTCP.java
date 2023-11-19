08f01a0fec0770dae54a5ffa54ce40d3af541c3e
Dennis Gallina <dennis.gallina@gmail.com>
2023-11-19 23:43:10 +0100

    Modifiche: - Migliorata sintassi; - Aggiunto il metodo che invia la richiesta di connessione; - Aggiunto il metodo che controlla se la connessione sia ancora attiva.

--- Client/src/ClientTCP.java
+++ Client/src/ClientTCP.java
@@ -5,28 +5,34 @@ import java.io.PrintWriter;
 import java.net.Socket;
 
 public class ClientTCP extends Thread{
-    private Socket socket; // Connessione del Client
+    private Socket connection; // Connessione del Client
     private BufferedReader input; // Ricezione Dati
     private PrintWriter output; // Invio Dati
     public String serverResponse; // Risposta ricevuta dal Server
 
     public ClientTCP(String serverIp, int serverPort) throws IOException {
-        this.socket = new Socket(serverIp, serverPort);
-        this.input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        this.output = new PrintWriter(socket.getOutputStream(), true);
+        this.connection = new Socket(serverIp, serverPort); // Aggiunta dei dati del Server
+        this.input = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        this.output = new PrintWriter(connection.getOutputStream(), true);
     }
 
     public void run() {
-        // Resta in ascolto del Server finché la Connessione non viene chiusa
-        while (!socket.isClosed()) {
+        // Resta in ascolto del Server finché la Connessione è attiva
+        while (isConnected()) {
             try {
                 serverResponse = input.readLine(); // Riceve dati dal Server
+                if (serverResponse != null)
+                    System.out.println("Risposta dal Server:" + serverResponse);
             } catch (IOException e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
             }
         }
     } 
+
+    public void connect() throws IOException {
+        send("connect");
+    }
 
     // Invia dei dati al Server
     public void send(String data) throws IOException {
@@ -37,6 +43,17 @@ public class ClientTCP extends Thread{
     public void close() throws IOException {
         input.close();
         output.close();
-        socket.close();
+        connection.close();
+    }
+
+    public boolean isConnected() {
+        if (connection.isConnected())
+            return true;
+        else 
+            return false;
+    }
+
+    public void cleanResponse() {
+        serverResponse = null;
     }
 }
