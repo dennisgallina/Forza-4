3187da93c99069fb495052ffc36268e6ed75bc5e
Dennis Gallina <dennis.gallina@gmail.com>
2023-11-19 23:47:19 +0100

    Sostituzione List con una matrice.

--- Client/src/PlayGround.java
+++ Client/src/PlayGround.java
@@ -1,22 +1,15 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class PlayGround {
-    public List<Pawn> pawns; // Pedine all'interno della griglia
     public int columns; // Numero di colonne della griglia
     public int rows; // Numero di righe della griglia
+    public Pawn[][] pawns; // Pedine all'interno della griglia
 
     public PlayGround() {
-        this.pawns = new ArrayList<>();
         this.columns = 6;
         this.rows = 7;
+        this.pawns = new Pawn[this.rows][this.columns];
     }
 
     public void insert(Pawn pawn) {
-        pawns.add(pawn);
-    }
-
-    public void clear() {
-        pawns.clear();
+        pawns[pawn.positionY][pawn.positionX] = pawn;
     }
 }
--- Server/src/PlayGround.java
+++ Server/src/PlayGround.java
@@ -1,27 +1,28 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class PlayGround {
-    public List<Pawn> pawns; // Pedine all'interno della griglia
-    public int[] columnHeights; // Altezza di ogni colonna
+    public static final String COLOR_RED = "red";
+    public static final String COLOR_YELLOW = "yellow";
+
     public int columns; // Numero di colonne della griglia
     public int rows; // Numero di righe della griglia
+    public Pawn[][] pawns; // Pedine all'interno della griglia
+    public int[] columnHeights; // Altezza di ogni colonna
 
     public PlayGround() {
-        this.pawns = new ArrayList<>();
-        this.columnHeights = new int[7];
         this.columns = 6;
         this.rows = 7;
+        this.pawns = new Pawn[this.rows][this.columns];
+        this.columnHeights = new int[this.rows];
     }
 
     // Inserisce la Pawn nel PlayGround
     public synchronized boolean insert(String color, int positionXPawn) {
-        if (isColumnFull(positionXPawn))
+        if (isColumnFull(positionXPawn)) {
             return false;
+        }
 
         // Creazione e inserimento Pawn nel PlayGround
         Pawn pawn = new Pawn(color, positionXPawn, columnHeights[positionXPawn]);
-        pawns.add(pawn);
+        pawns[pawn.positionY][pawn.positionX] = pawn;
         columnHeights[positionXPawn]++;
         return true;
     }
@@ -34,26 +35,34 @@ public class PlayGround {
     // Questo metodo controlla se un giocatore ha vinto
     public String checkWin() {
         // Scorre tutte le pedine
-        for (Pawn pawn : pawns) {
-            String color = pawn.color;
-            int posX = pawn.positionX;
-            int posY = pawn.positionY;
+        for (Pawn[] row : pawns) {
+            for (Pawn pawn : row) {
+                if (pawn != null) {
+                    String color = pawn.color;
+                    int posX = pawn.positionX;
+                    int posY = pawn.positionY;
 
-            // Controlla se ci sono 4 pedine dello stesso colore in fila orizzontalmente
-            if (checkLine(color, posX, posY, 1, 0) >= 4)
-                return color;
+                    // Controlla se ci sono 4 pedine dello stesso colore in fila orizzontalmente
+                    if (checkLine(color, posX, posY, 1, 0) >= 4) {
+                        return color;
+                    }
 
-            // Controlla se ci sono 4 pedine dello stesso colore in fila verticalmente
-            if (checkLine(color, posX, posY, 0, 1) >= 4)
-                return color;
+                    // Controlla se ci sono 4 pedine dello stesso colore in fila verticalmente
+                    if (checkLine(color, posX, posY, 0, 1) >= 4) {
+                        return color;
+                    }
 
-            // Controlla se ci sono 4 pedine dello stesso colore in fila diagonalmente da sinistra a destra
-            if (checkLine(color, posX, posY, 1, 1) >= 4)
-                return color;
+                    // Controlla se ci sono 4 pedine dello stesso colore in fila diagonalmente da sinistra a destra
+                    if (checkLine(color, posX, posY, 1, 1) >= 4) {
+                        return color;
+                    }
 
-            // Controlla se ci sono 4 pedine dello stesso colore in fila diagonalmente da destra a sinistra
-            if (checkLine(color, posX, posY, 1, -1) >= 4)
-                return color;
+                    // Controlla se ci sono 4 pedine dello stesso colore in fila diagonalmente da destra a sinistra
+                    if (checkLine(color, posX, posY, 1, -1) >= 4) {
+                        return color;
+                    }
+                }
+            }
         }
         // Se nessuna delle condizioni sopra è vera, nessun giocatore ha vinto
         return null;
@@ -65,11 +74,12 @@ public class PlayGround {
         // Controlla le 4 posizioni nella direzione specificata
         for (int i = 0; i < 4; i++) {
             // Se la pedina in questa posizione è dello stesso colore, incrementa il contatore
-            if (getPawn(posX + dirX * i, posY + dirY * i).color.equals(color))
+            if (getPawn(posX + dirX * i, posY + dirY * i).color.equals(color)) {
                 count++;
-            else
+            } else {
                 // Se la pedina non è dello stesso colore, interrompe il ciclo
                 break;
+            }
         }
         // Restituisce il numero di pedine dello stesso colore in fila
         return count;
@@ -77,13 +87,9 @@ public class PlayGround {
 
     // Restituisce la pedina in una certa posizione
     private Pawn getPawn(int posX, int posY) {
-        // Scorre tutte le pedine
-        for (Pawn pawn : pawns) {
-            // Se la pedina è nella posizione specificata, la restituisce
-            if (pawn.positionX == posX && pawn.positionY == posY)
-                return pawn;
+        if (posX >= 0 && posX < columns && posY >= 0 && posY < rows) {
+            return pawns[posY][posX];
         }
-        
         return null;
     }
 
@@ -98,15 +104,15 @@ public class PlayGround {
                 // Ottiene la pedina nella posizione corrente
                 Pawn pawn = getPawn(x, y);
                 // Se la posizione è vuota, aggiunge '0' alla stringa
-                if (pawn.color == null) {
+                if (pawn == null) {
                     sb.append('0'); // Casella vuota
                 } 
                 // Se la posizione contiene una pedina rossa, aggiunge '1' alla stringa
-                else if (pawn.color.equals("red")) {
+                else if (COLOR_RED.equals(pawn.color)) {
                     sb.append('1'); // Pedina rossa
                 } 
                 // Se la posizione contiene una pedina gialla, aggiunge '2' alla stringa
-                else if (pawn.color.equals("yellow")) {
+                else if (COLOR_YELLOW.equals(pawn.color)) {
                     sb.append('2'); // Pedina gialla
                 }
             }
@@ -117,4 +123,3 @@ public class PlayGround {
         return sb.toString();
     }
 }
-
